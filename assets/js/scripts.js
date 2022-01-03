de7d63d11634d80fdd2e35bad6e5968450d15952
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2022-01-03 16:49:29 +0530

    first

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,9 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("myImg")[0];
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+$(function () {
+  $(".pop").on("click", function () {
+    $(".imagepreview").attr("src", $(this).find("img").attr("src"));
+    $("#imagemodal").modal("show");
+  });
+});
 
 const video = document.getElementById("video");
 
